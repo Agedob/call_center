a0b344f8bc44183b17ac33eaf5b144378d715e06
agedob <agedob888@gmail.com>
2018-02-07 20:17:39 -0800

    inital commit

---
+++ server.py
@@ -0,0 +1,33 @@
+class call(object):
+    def __init__(self, id, name, number , time, reason):
+        self.id = id
+        self.name = name
+        self.number = number
+        self.time = time
+        self.reason = reason
+
+    def display_all(self):
+        print self.id
+        print self.name = name
+        print self.number = number
+        print self.time = time
+        print self.reason = reason
+        return self
+
+class call_center(object):
+    def __init__(self, calls, que):
+        self.call = call
+        self.que = que
+
+    def add():
+        
+    return self
+
+    def remove():
+    
+    return self
+
+    def info():
+
+    return self
+
